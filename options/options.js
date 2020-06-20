5a379e2ada3e933e901bcd0575e6f1debdce0be8
Rudie Dirkx <github@hotblocks.nl>
2020-06-20 21:36:19 +0200

    editing table+

--- options/options.js
+++ options/options.js
@@ -1,6 +1,8 @@
 
 function init() {
 
+	console.time('init');
+
 	function flatten(tree, location = []) {
 		var items = [];
 		tree.forEach(function(item) {
@@ -23,7 +25,7 @@ function init() {
 	var table = document.querySelector('#bookmarks-table');
 	var form = document.querySelector('#bookmarks-form');
 
-	var bookmarks;
+	// var bookmarks;
 
 	form.onsubmit = function(e) {
 		e.preventDefault();
@@ -42,10 +44,12 @@ function init() {
 			save[id][name] = element.value;
 		});
 
+		console.log(save);
+
 		var num = Object.keys(save).length;
-		var message = "You will save " + num + " bookmarks.\n\nAgree?";
+		var message = "You will save " + num + " bookmarks. See the console.\n\nAgree?";
 		if (confirm(message)) {
-			for ( var id in save ) {
+			for (var id in save) {
 				chrome.bookmarks.update(id, save[id], function() {
 					if (--num == 0) {
 						location.reload();
@@ -64,24 +68,34 @@ function init() {
 
 		document.querySelector('#num-bookmarks').textContent = items.length;
 
-		bookmarks = items.reduce(function(list, item) {
-			list[item.id] = item;
-			return list;
-		}, {});
+		// bookmarks = items.reduce(function(list, item) {
+		// 	list[item.id] = item;
+		// 	return list;
+		// }, {});
 
-		var html = items.map(function(item) {
-			return '' +
+		const countChildren = location => items.filter(item => item.location == location).length;
+
+		var lastLoc = '';
+		var html = '';
+		for (let item of items) {
+			if (item.location != lastLoc) {
+				lastLoc = item.location;
+				html += '<tr><th colspan="2">' + _html(item.location) + ' (' + countChildren(item.location) + ')</th></tr>';
+			}
+
+			html +=
 				'<tr>' +
-					'<td class="location">' + _html(item.location) + '</td>' +
+					// '<td class="location">' + _html(item.location) + '</td>' +
 					'<td class="title"><input data-name="title.' + item.id + '" value="' + _html(item.title) + '" /></td>' +
 					'<td class="url"><input data-name="url.' + item.id + '" value="' + _html(item.url) + '" /></td>' +
 				'</tr>';
-		}).join('');
+		}
 		table.innerHTML = html;
 
 		document.body.classList.remove('loading');
-	});
 
+		console.timeEnd('init');
+	});
 }
 
 window.onload = init;
